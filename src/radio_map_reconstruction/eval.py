6bb62106d7c3f258e4f32e907e06b93985558d60
lzj <2575634450@qq.com>
2026-08-23 11:18:24 +0800

    finished eval

---
+++ src/radio_map_reconstruction/eval.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from os.path import join
+from torch import load
+from torch.nn import Module
+from yaml import safe_load
+from radio_map_reconstruction.data import RadioDataset
+from radio_map_reconstruction.loss import RadioMapLoss
+from radio_map_reconstruction.model import ResUnet
+from torch.utils.data import DataLoader
+
+from radio_map_reconstruction.train import eval_one_epoch
+
+ROOT = Path(__file__).resolve().parents[2]
+CONFIG_PATH = join(ROOT, "config.yml")
+with open(CONFIG_PATH, encoding="utf-8") as file:
+    CONFIG = safe_load(file)
+
+def load_checkpoint(path: str) -> Module:
+    checkpoint = load(path, map_location=CONFIG["device"])
+    model = ResUnet().to(CONFIG["device"])
+    model.load_state_dict(checkpoint["model_state_dict"])
+    return model
+
+
+def eval():
+    test_dataset = RadioDataset("test")
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        num_workers=0,
+        batch_size=4,
+        shuffle=True,
+        pin_memory=True
+    )
+
+    best_checkpoint_path = join(ROOT, "run", "best.pt")
+    model = load_checkpoint(best_checkpoint_path)
+    criterion = RadioMapLoss()
+    
+    test_loss = eval_one_epoch(model, test_loader, criterion, 0, 1)
+
+    print(f"test loss: {test_loss:.6f}")
+
+    
--- tests/radio_map_reconstruction/test_train.py
+++ tests/radio_map_reconstruction/test_train.py
@@ -10,6 +10,7 @@ from torch.utils.data import DataLoader, Subset
 from radio_map_reconstruction.train import eval_one_epoch, save_checkpoint, train_one_epoch
 from radio_map_reconstruction.util import delete_run
 from radio_map_reconstruction.split import split
+from radio_map_reconstruction.eval import eval
 
 ROOT = Path(__file__).resolve().parents[2]
 CONFIG_PATH = join(ROOT, "config.yml")
@@ -54,15 +55,21 @@ def test_train_one_epoch():
     criterion = RadioMapLoss()
     optimizer = Adam(res_unet.parameters(), lr=3e-4)
 
+    lowest_val_loss = float("inf")
     train_loss = train_one_epoch(res_unet, train_loader, optimizer, criterion, 0, 1)
     val_loss = eval_one_epoch(res_unet, val_loader, criterion, 0, 1)
 
     last_checkpoint_path = join(ROOT, "run", "last.pt")
     save_checkpoint(last_checkpoint_path, res_unet)
+    if val_loss < lowest_val_loss:
+        lowest_val_loss = val_loss
+        best_checkpoint_path = join(ROOT, "run", "best.pt")
+        save_checkpoint(best_checkpoint_path, res_unet)
 
     swanlab.log({
         "train_loss": train_loss,
         "val_loss": val_loss
     })
     swanlab.finish()
-    # delete_run()
+    
+    delete_run()
